c4dbf89347968d4f9aa3c9cb4a10d92ccd84103f
= <your.email@example.com>
2024-10-04 10:15:59 +0100

    file

---
+++ sessions/python/file.py
@@ -0,0 +1,16 @@
+#fp = open(r'C:\Users\Alin\Desktop\SD course\sessions\python\sample\data.txt ', 'r')
+#print(fp.read())
+#fp.close()
+
+
+
+fp = open('sample/file1.txt', 'x')
+fp.write('firstline')
+fp.close()
+
+
+
+
+with  open(r'sample/data.txt, 'a') as fp:
+  fp.write("This will go last")
+
